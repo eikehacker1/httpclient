d363b1ed626e4c745324b7aa036b6d7463a4dead
EIKE Oliveira <101583887+eikehacker1@users.noreply.github.com>
2023-10-11 10:01:29 -0300

    Update portscanner.go

--- portscanner/portscanner.go
+++ portscanner/portscanner.go
@@ -2,40 +2,26 @@
 package portscanner
 
 import (
-    "fmt"
-    "net"
-    "strconv"
-    "strings"
+	"net"
+	"strconv"
 )
 
-// ScanPorts verifica a abertura de portas e verifica se há serviços HTTP.
-func ScanPorts(host string, ports []int) {
-    for _, port := range ports {
-        target := fmt.Sprintf("%s:%d", host, port)
-        conn, err := net.Dial("tcp", target)
-        if err != nil {
-            fmt.Printf("%s [Port %d]: Closed\n", host, port)
-            continue
-        }
-        defer conn.Close()
-
-        fmt.Printf("%s [Port %d]: Open\n", host, port)
-
-        // Você pode adicionar aqui a lógica para verificar se há um serviço HTTP em execução na porta.
-    }
+func IsPortOpen(host, port string) bool {
+	conn, err := net.Dial("tcp", host+":"+port)
+	if err != nil {
+		return false
+	}
+	defer conn.Close()
+	return true
 }
 
-// ParsePortList analisa uma lista de portas no formato "80,443,8080".
-func ParsePortList(portsStr string) []int {
-    portStrings := strings.Split(portsStr, ",")
-    ports := []int{}
-    for _, portStr := range portStrings {
-        port, err := strconv.Atoi(portStr)
-        if err != nil {
-            fmt.Printf("Error parsing port %s: %v\n", portStr, err)
-            continue
-        }
-        ports = append(ports, port)
-    }
-    return ports
+func ScanPorts(host string, ports []int) []int {
+	openPorts := []int{}
+	for _, port := range ports {
+		portStr := strconv.Itoa(port)
+		if IsPortOpen(host, portStr) {
+			openPorts = append(openPorts, port)
+		}
+	}
+	return openPorts
 }
