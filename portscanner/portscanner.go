a9f2ec904d476e8cea75530c84ca2b4084c3507c
EIKE Oliveira <101583887+eikehacker1@users.noreply.github.com>
2023-10-11 10:10:20 -0300

    Update portscanner.go

--- portscanner/portscanner.go
+++ portscanner/portscanner.go
@@ -2,26 +2,39 @@
 package portscanner
 
 import (
-	"net"
-	"strconv"
+    "strconv"
+    "strings"
 )
 
-func IsPortOpen(host, port string) bool {
-	conn, err := net.Dial("tcp", host+":"+port)
-	if err != nil {
-		return false
-	}
-	defer conn.Close()
-	return true
-}
-
-func ScanPorts(host string, ports []int) []int {
-	openPorts := []int{}
-	for _, port := range ports {
-		portStr := strconv.Itoa(port)
-		if IsPortOpen(host, portStr) {
-			openPorts = append(openPorts, port)
-		}
-	}
-	return openPorts
+func ParsePortList(portsStr string) []int {
+    portList := []int{}
+    portRanges := strings.Split(portsStr, ",")
+    for _, pr := range portRanges {
+        if strings.Contains(pr, "-") {
+            // Handle port ranges, e.g., "80-85"
+            rangeParts := strings.Split(pr, "-")
+            if len(rangeParts) != 2 {
+                continue
+            }
+            startPort, err := strconv.Atoi(rangeParts[0])
+            if err != nil {
+                continue
+            }
+            endPort, err := strconv.Atoi(rangeParts[1])
+            if err != nil {
+                continue
+            }
+            for p := startPort; p <= endPort; p++ {
+                portList = append(portList, p)
+            }
+        } else {
+            // Handle single ports, e.g., "80"
+            port, err := strconv.Atoi(pr)
+            if err != nil {
+                continue
+            }
+            portList = append(portList, port)
+        }
+    }
+    return portList
 }
